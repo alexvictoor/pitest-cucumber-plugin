4ae94525d11911b1b2316eb90e6a53da1cd43aa3
alexvictoor <alexvictoor@gmail.com>
2014-09-04 23:58:51 +0200

    Fix bug when cucumber not used

--- src/main/java/org/pitest/cucumber/CucumberJUnitCompatibleConfiguration.java
+++ src/main/java/org/pitest/cucumber/CucumberJUnitCompatibleConfiguration.java
@@ -5,6 +5,7 @@ import org.pitest.junit.CompoundTestUnitFinder;
 import org.pitest.junit.JUnitCompatibleConfiguration;
 import org.pitest.testapi.TestGroupConfig;
 import org.pitest.testapi.TestUnitFinder;
+import org.pitest.util.Log;
 
 import java.util.List;
 
@@ -18,8 +19,26 @@ public class CucumberJUnitCompatibleConfiguration extends JUnitCompatibleConfigu
 
     @Override
     public TestUnitFinder testUnitFinder() {
-        List<TestUnitFinder> finders
-                = asList(new CucumberTestUnitFinder(), super.testUnitFinder());
-        return new CompoundTestUnitFinder(finders);
+        final TestUnitFinder finder;
+        if (isCucumberUsed()) {
+            Log.getLogger().info("Cucumber detected, scenarios will be used");
+            List<TestUnitFinder> finders
+                    = asList(new CucumberTestUnitFinder(), super.testUnitFinder());
+            finder = new CompoundTestUnitFinder(finders);
+        } else {
+            Log.getLogger().info("Cucumber not used in this project");
+            finder = super.testUnitFinder();
+        }
+        return finder;
+    }
+
+    private boolean isCucumberUsed() {
+        boolean result = false;
+        try {
+            Class.forName("cucumber.api.junit.Cucumber");
+            result = true;
+        } catch (ClassNotFoundException e) {
+        }
+        return result;
     }
 }
