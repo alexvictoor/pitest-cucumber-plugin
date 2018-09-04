9b8706011de739ec44425c1fe04066fd25a6c8b4
FelixLee888 <felixlee@hotmail.com>
2018-09-04 10:42:31 +0800

    Update to support PIT 1.4.3

--- src/main/java/org/pitest/cucumber/CucumberTestFrameworkPlugin.java
+++ src/main/java/org/pitest/cucumber/CucumberTestFrameworkPlugin.java
@@ -4,6 +4,7 @@ import org.pitest.classinfo.ClassByteArraySource;
 import org.pitest.testapi.Configuration;
 import org.pitest.testapi.TestGroupConfig;
 import org.pitest.testapi.TestPluginFactory;
+import org.pitest.util.Preconditions;
 
 import java.util.Collection;
 
@@ -18,6 +19,17 @@ public class CucumberTestFrameworkPlugin implements TestPluginFactory {
                                                           Collection<String> excludedRunners) {
 
         return new CucumberJUnitCompatibleConfiguration(config);
+    }
+
+    @Override
+    public Configuration createTestFrameworkConfiguration(TestGroupConfig config, ClassByteArraySource source, Collection<String> excludedRunners, Collection<String> includedTestMethods) {
+        Preconditions.checkNotNull(config);
+        return new CucumberJUnitCompatibleConfiguration(config);
+    }
+
+    @Override
+    public String name() {
+        return "Cucumber";
     }
 
 }
